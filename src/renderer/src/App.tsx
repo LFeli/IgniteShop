7474f612f758dfdc95b66d15009031ed316831ed
Lucas Felipe <lucasfelipemk@gmail.com>
2024-03-30 20:16:05 -0300

    :sparkles: feat: Config global colors and styles

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/global.css'
 
 export function App() {
-  return <h1 className="text-4xl font-bold text-violet-400">Hello World</h1>
+  return <div className="bg-rotion-900 text-rotion-100 h-screen w-screen"></div>
 }
