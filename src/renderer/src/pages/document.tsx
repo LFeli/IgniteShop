35ba0ffad251a2d072509bedde49f54ec37de304
Lucas Felipe <lucasfelipemk@gmail.com>
2024-03-31 09:33:33 -0300

    :sparkles: feat: Create a base of structure of document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,21 @@
-import { Link } from 'react-router-dom'
+import { ToC } from '../components/ToC'
 
 export function Document() {
   return (
-    <main className="flex flex-1 items-center justify-center text-rotion-400">
-      Selecione ou crie um documento
-      <Link to="/">Acessar Blank</Link>
+    <main className="flex flex-1 gap-8 px-10 py-12">
+      <aside className="sticky top-0 hidden lg:block">
+        <span className="text-xs font-semibold uppercase text-rotion-300">
+          TABLE OF CONTENTS
+        </span>
+
+        <ToC.Root>
+          <ToC.Link> Back-end </ToC.Link>
+          <ToC.Section> Banco de Dados </ToC.Section>
+          <ToC.Section> Autenticação </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex flex-1 flex-col items-center">Content</section>
     </main>
   )
 }
