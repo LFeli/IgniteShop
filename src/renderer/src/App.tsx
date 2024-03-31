1334337e7c3eb5e9a2154440b3fcc536e361cb99
Lucas Felipe <lucasfelipemk@gmail.com>
2024-03-31 08:54:31 -0300

    :wrench: chore: Config react-router-dom and firsts routes for project

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,17 +2,16 @@ import './styles/global.css'
 
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
+import { Routes } from './routes'
 
 export function App() {
   return (
-    <div className="bg-rotion-900 text-rotion-100 flex h-screen w-screen">
+    <div className="flex h-screen w-screen bg-rotion-900 text-rotion-100">
       <Sidebar />
       <div className="flex max-h-screen flex-1 flex-col">
         <Header />
 
-        <main className="text-rotion-400 flex flex-1 items-center justify-center">
-          Selecione ou crie um documento
-        </main>
+        <Routes />
       </div>
     </div>
   )
