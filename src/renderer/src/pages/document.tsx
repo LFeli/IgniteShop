74f4281f39c652d19ff28abb7025cb03d9b8ffef
Lucas Felipe <lucasfelipemk@gmail.com>
2024-03-31 09:59:45 -0300

    :sparkles: feat: Add Tiptap editor in the project on document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,3 +1,4 @@
+import { Editor } from '../components/Editor'
 import { ToC } from '../components/ToC'
 
 export function Document() {
@@ -15,7 +16,9 @@ export function Document() {
         </ToC.Root>
       </aside>
 
-      <section className="flex flex-1 flex-col items-center">Content</section>
+      <section className="flex flex-1 flex-col items-center">
+        <Editor />
+      </section>
     </main>
   )
 }
