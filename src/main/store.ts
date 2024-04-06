05ba064fd4c8993786b6a152e5354a1c058f1393
Lucas Felipe <lucasfelipemk@gmail.com>
2024-04-06 15:41:15 -0300

    :sparkles: feat: Create conections with store

--- src/main/store.ts
+++ src/main/store.ts
@@ -1,7 +1,8 @@
+import { Document } from '@shared/types/ipc'
 import Store from 'electron-store'
 
 interface StoreType {
-  documents: Record<string, any>
+  documents: Record<string, Document>
 }
 
 export const store = new Store<StoreType>({
