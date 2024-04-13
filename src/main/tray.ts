3813e0448d899f19c1a2e682ff4d3e877c7bf7d3
Lucas Felipe <lucasfelipemk@gmail.com>
2024-04-13 15:59:46 -0300

    :sparkles: feat: Create a Tray menu

---
+++ src/main/tray.ts
@@ -0,0 +1,19 @@
+import path from 'node:path'
+
+import { app, Menu, Tray } from 'electron'
+
+app.whenReady().then(() => {
+  const tray = new Tray(path.resolve(__dirname, 'rotionTemplate.png'))
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Rotion', enabled: false },
+    { type: 'separator' },
+    { type: 'checkbox', label: 'Ativar modo dark' },
+    { label: 'Rotion' },
+    { label: 'Rotion' },
+    { label: 'Rotion' },
+    { label: 'Rotion' },
+  ])
+
+  tray.setContextMenu(contextMenu)
+})
